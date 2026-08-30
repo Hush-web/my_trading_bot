f1701ce9aef870534caa39a555c7dbbba77cbf33
Hush-web <hushweb@example.com>
2026-08-31 00:04:46 +0100

    Overwrite SecureFreqStrategy with mean-reversion scalper

--- user_data/strategies/SecureFreqStrategy.py
+++ user_data/strategies/SecureFreqStrategy.py
@@ -1,50 +1,60 @@
-﻿from freqtrade.strategy import IStrategy, IntParameter
+﻿from freqtrade.strategy import IStrategy, IntParameter, DecimalParameter
 from pandas import DataFrame
 import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
 
 class SecureFreqStrategy(IStrategy):
     timeframe = '5m'
-    startup_candle_count = 200
+    startup_candle_count: int = 200
 
     max_open_trades = 3
     stake_amount = 50
-    stoploss = -0.03
+
+    stoploss = -0.025
 
     trailing_stop = True
-    trailing_stop_positive = 0.01
+    trailing_stop_positive = 0.008
     trailing_stop_positive_offset = 0.015
     trailing_only_offset_is_reached = True
 
     minimal_roi = {
-        "0": 0.05,
-        "30": 0.03,
-        "60": 0.01,
-        "120": 0.0
+        "0": 0.02,
+        "15": 0.01,
+        "30": 0.005,
+        "60": 0.0
     }
 
-    buy_ema_short = IntParameter(5, 15, default=9, space='buy')
-    buy_ema_long = IntParameter(15, 30, default=21, space='buy')
-    buy_rsi = IntParameter(45, 60, default=50, space='buy')
-    sell_rsi = IntParameter(45, 60, default=50, space='sell')
+    buy_rsi = IntParameter(20, 35, default=28, space='buy')
+    sell_rsi = IntParameter(55, 75, default=65, space='sell')
+    bb_std = DecimalParameter(1.5, 2.5, default=2.0, space='buy')
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe['ema_short'] = ta.EMA(dataframe, timeperiod=self.buy_ema_short.value)
-        dataframe['ema_long'] = ta.EMA(dataframe, timeperiod=self.buy_ema_long.value)
+        # Bollinger Bands
+        bollinger = ta.BBANDS(
+            dataframe['close'],
+            timeperiod=20,
+            nbdevup=self.bb_std.value,
+            nbdevdn=self.bb_std.value,
+            matype=0  # SMA
+        )
+        dataframe['bb_upper'] = bollinger['upperband']
+        dataframe['bb_middle'] = bollinger['middleband']
+        dataframe['bb_lower'] = bollinger['lowerband']
+
         dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
         dataframe['atr'] = ta.ATR(dataframe, timeperiod=14)
-        dataframe['atr_percent'] = (dataframe['atr'] / dataframe['close']) * 100
+
         dataframe['volume_mean'] = dataframe['volume'].rolling(20).mean()
-        dataframe['volume_ratio'] = dataframe['volume'] / dataframe['volume_mean']
+        dataframe['volume_spike'] = dataframe['volume'] / dataframe['volume_mean']
+
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                (dataframe['ema_short'] > dataframe['ema_long']) &
-                (dataframe['rsi'] > self.buy_rsi.value) &
-                (dataframe['atr_percent'] < 3.0) &
-                (dataframe['volume_ratio'] > 0.8)
+                (dataframe['rsi'] < self.buy_rsi.value) &
+                (dataframe['close'] <= dataframe['bb_lower']) &
+                (dataframe['volume_spike'] > 1.2) &
+                (dataframe['atr'] / dataframe['close'] < 0.03)
             ),
             'buy'] = 1
         return dataframe
@@ -52,8 +62,8 @@ class SecureFreqStrategy(IStrategy):
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                (dataframe['ema_short'] < dataframe['ema_long']) |
-                (dataframe['rsi'] < self.sell_rsi.value)
+                (dataframe['rsi'] > self.sell_rsi.value) |
+                (dataframe['close'] >= dataframe['bb_middle'])
             ),
             'sell'] = 1
         return dataframe
